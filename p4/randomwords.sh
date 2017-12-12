3c69c32e1ae99c8d9469ce4119384bb6380702e3
Will Hegedus <wbhegedus@gmail.com>
2017-12-12 09:26:34 -0500

    p4 finished

--- p4/randomwords.sh
+++ p4/randomwords.sh
@@ -1,12 +1,11 @@
 numWords=4
 variance=0
 subs='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890'
-imLazy=`echo 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890' | grep -o .`
+imLazy=($(echo 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890' | grep -o .))
 
 function getWord() {
   wget -O index.html -q https://randomword.com/
-  cat index.html | grep '"random_word">' | sed 's/<div id="random_word">//' | sed 's/<\/div>//' |
-  xargs >> words.txt
+  cat index.html | grep '"random_word">' | sed 's/<div id="random_word">//' | sed 's/<\/div>//' | xargs >> words.txt
 }
 
 
@@ -19,31 +18,38 @@ function argParse() {
       fi;;
 
     t)
-      if [[ $OPTARG -ge 0 ]] && [[ $OPTARG -le 1 ]]; then
-        variance=$OPTARG
+      var1=$OPTARG
+      var2=`echo $var1 \* 100 | bc -l`
+      variance=${var2%.*}
+      if [[ $variance -ge 0 ]] && [[ $variance -le 100 ]]; then
+        :
+      else
+        echo "Invalid variance. Please provide float between 0.0 and 1.0"
       fi;;
-    esac
+   
+   esac
   done
 }
 
 function substitutes () {
-  echo "subbing"
   sed ':a;N;$!ba;s/\n//g' words.txt > word.txt
   array=($(cat word.txt | grep -o .))
-  echo ${array[*]} "is array"
-    for ((i=0 ; i<${#words[@]}; i++)) ; do
-        if [[ $variance -ge $RANDOM ]]; then
+    for ((i=0 ; i<${#array[@]}; i++)) ; do
+        rando=`shuf -i 0-100 -n 1`
+        if [[ $variance -ge $rando ]]; then
           change=`shuf -i 1-62 -n 1`
-          array[$i]=imLazy[$change]
+          array[$i]=${imLazy[$change]}
         fi
-    echo ${array[2]}
     done
+  rm words.txt
+  echo ${array[*]} > word.txt
+  sed -i "s/ //g" word.txt
+  cat word.txt
 }
 
 function allTheWords () {
   x=1
   while [ $x -le $numWords ]; do
-    echo "word retrieval"
     getWord
     x=$(( $x + 1 ))
   done
